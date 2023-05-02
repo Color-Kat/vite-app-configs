f2d57bcd90ae14c0b2f7f226fbbdc295e1000596
Color-Kat <katik7979@gmail.com>
2023-05-02 21:18:39 +0300

    react+ts

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Vite-app-config
+Here's my config of vite app with different modules (react, ts, tailwind, scss, etc.).
+You can find all config in commits.
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from "path";
+
+export default defineConfig({
+  build: {
+    outDir: path.join(__dirname, "public"),
+
+  },
+  base: './',
+  plugins: [
+    react({}),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+      '@hooks': path.resolve(__dirname, 'src/hooks/'),
+      '@assets': path.resolve(__dirname, 'src/assets/'),
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@pages': path.resolve(__dirname, 'src/pages/'),
+      '@UI': path.resolve(__dirname, 'src/UI/'),
+      '@utils': path.resolve(__dirname, 'src/utils/')
+    },
+    extensions: ['.js', '.ts', '.tsx', '.jsx'],
+  },
+});
