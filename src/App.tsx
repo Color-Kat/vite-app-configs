0bbeb873a6a129373e643728672550847088e481
Color-Kat <katik7979@gmail.com>
2023-05-03 17:19:34 +0300

    project structure, redux toolkit start, browser-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import {useState} from 'react';
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <>
+        <BrowserRouter>
             <h1 className="text-2xl font-bold">Color config</h1>
             <div className="font-roboto">
-                <button onClick={() => setCount((count) => count + 1)}>
+                <button onClick={() => setCount((count) => count - 1)}>
                     count is {count}
                 </button>
                 <p>
@@ -17,7 +18,7 @@ function App() {
             <p className="">
                 Click on the Vite and React logos to learn more
             </p>
-        </>
+        </BrowserRouter>
     );
 }
 
