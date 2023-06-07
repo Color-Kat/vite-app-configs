30f9f4d7f12079be2d99d213d8a840ee338ebb22
Color-Kat <katik7979@gmail.com>
2023-06-07 21:02:44 +0300

    some fixes

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { Provider } from "react-redux";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App.tsx';
+import App from './App';
 import { store } from "@/store";
 import './index.scss';
 
